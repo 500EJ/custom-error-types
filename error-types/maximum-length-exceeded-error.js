e5cf388db0b903ecfecb9b070bfdb770c8f6a9b8
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-07-31 18:29:19 -0400

    Implement MaximumLengthExceededError

--- error-types/maximum-length-exceeded-error.js
+++ error-types/maximum-length-exceeded-error.js
@@ -1,6 +1,15 @@
 const ValidationError = require("./validation-error");
 
-// Your code here
+class MaximumLengthExceededError extends ValidationError {
+  constructor(amountExceeded, ...params) {
+    super(...params);
+
+    this.name = "MaximumLengthExceededError";
+    this.message = amountExceeded
+      ? `Maximum length exceeded by ${amountExceeded}`
+      : "Maximum length exceeded";
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
